66a47e285b39d3b00e436e41415abe6578835cf5
Luia Yen <luia.yen@gmail.com>
2016-08-24 17:50:42 -0700

    added below_absolute_zero boolean test

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -2,7 +2,7 @@
 
 def convert(temp, measure)
   return "Temperature must be an integer" unless temp.class == Fixnum
-  return "Temperature below Absolute Zero" if temp < -474
+  return "Temperature below Absolute Zero" if below_absolute_zero?(temp,measure) == true
   if measure == "F"
     5 * (temp - 32)/9
   else
@@ -10,12 +10,18 @@ def convert(temp, measure)
   end
 end
 
-# puts convert(32)
-# puts convert(50)
-# puts convert("zero")
-# puts convert(212)
-# puts convert(-500) 
+def below_absolute_zero?(temp,measure)
+  (temp < -454 and measure == "F") or (temp < -270 and measure == "C")
+end
 
+# Fahrenheit conversions to Celsius
+puts convert(32,"F")
+puts convert(50,"F")
+puts convert("zero","F")
+puts convert(212,"F")
+puts convert(-500,"F") 
+
+# Celsius conversions to Fahrenheit
 puts convert(0,"C")
 puts convert(10,"C")
 puts convert(100,"C")
